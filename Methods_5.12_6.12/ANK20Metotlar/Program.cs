557c169679df66f96182bc7e6d71bdde0f2c3566
Rojbin Agtas <rojbinagtas@gmail.com>
2024-12-08 16:05:33 +0300

    Add files via upload

---
+++ Methods_5.12_6.12/ANK20Metotlar/Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+
+namespace Methods_5._12
+{
+    internal class Program
+    {
+        //Value türündeki bir parametre, metot içerisinde değişiklik yapılsa bile metottan çıktıktan sonra, yine eski değerini görürür. Buna  "call-by-value" denir.
+
+        //Reference türündeki bir parametre, metot içerisinde değişiklik yaplırsa metottan çıktıktan sonra, yeni değerini görürür. Buna  "call-by-reference" denir.
+        //string istisnadır. Reference tür olmasına rağmen, call-by-value gider.
+
+        //Value türlerini referance türler gibi göndermek için ref veya out kelimelerini kullanırız. İkisi de call-by-ref yapar. 
+        //AMA!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        //ref:Metod içerisinde değer atamaya GEREK YOK. AMA metodu çağırırken değer atamak ZORUNDAYIZ!!!!!!!!!!!!!!!!!!!
+
+        //out:Metod içerisinde değer atamaya GEREK VAR. AMA metodu çağırırken değer atamaya GEREK YOK!!!!!!!!!!!!!!!!!!!
+
+        //Aynı zamanda metodu çağırırken de parametre yerinde out ile değişken tanımlanabilir fakat ref ile tanımlamaz.
+        //(out ile olan bu değişkeni isterseniz parametre kısmında değil de yine öncesinde de tanımlanabilir)
+
+
+
+        static int F(int x)  //call-by-value
+        {
+            x++;
+            return x;
+        }
+        static int F2(ref int x)  //call-by-value
+        {
+            x++;
+            return x;
+        }
+        static int F3(out int x)  //call-by-value
+        {
+            x = 1;
+            x++;
+            return x;
+        }
+        static void F4(out string kelime)
+        {
+            kelime = "Sonuç";
+        }
+        static double Ornek1( double sayi,out int tam,out double ondalik)
+        {
+           
+            tam = (int)sayi;
+            ondalik = sayi - tam;
+           
+            return tam * tam - ondalik;
+        }
+        static void BasitCarpimTablosu(int sayi,out int birKati, out int ikiKati, out int ucKati, out int dortKati, out int besKati)
+        {
+            birKati = sayi * 1;
+            ikiKati = sayi * 2;
+            ucKati = sayi * 3;
+            dortKati = sayi * 4;
+            besKati = sayi * 5;
+        }
+
+        static void KDVliHesapla(ref float fiyat)
+        {
+            fiyat *= 1.2f;
+
+        }
+        static void Deneme(int y=100, int x = 9) //bu parametreli metodda x ve y'nin sıralarını ve nasıl eklenip nasıl sıralarının değiştirilebileceği anlatılıyor.
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+
+        static void Main(string[] args)
+        {
+            #region Deneme methodunun döndüreceği değerler ve aradaki fark
+            Deneme(13); //13,100
+            Deneme(13, 44); //13,44
+            #endregion
+
+            #region KDV'li hesap metodu
+            float price;
+            Console.WriteLine("Please write the price of the product:");
+            price = Convert.ToSingle(Console.ReadLine());
+            KDVliHesapla(ref price);
+            Console.WriteLine(price);
+            #endregion
+
+            #region Çarpım tablosu metodu
+            int sayi;
+            Console.WriteLine("Sayıyı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            BasitCarpimTablosu(sayi,out int birKati, out int ikiKati, out int ucKati, out int dortKati, out int besKati);
+            #endregion
+
+            #region Ondalıklı bir sayının tam ve ondalık kısmını veren metot
+            double sayi2;
+            Console.WriteLine("Sayıyı giriniz:");
+
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Önce Sayı: " + sayi2);
+
+            var sonuc2 = Ornek1(sayi2, out int tam, out double ondalik);
+            Console.WriteLine("Tam: " + tam + " Ondalik: " + ondalik);
+            Console.WriteLine("Sonuc: " + sonuc2);
+            Console.WriteLine("Sonra sayı: " + sayi2);
+            #endregion
+
+            #region F2 ve F4 metotları
+            string s = "5";
+            string word = "abc";
+            Console.WriteLine("önce: " + word);
+            F4(out word);
+            Console.WriteLine("sonra: " + word);
+
+            var durum = int.TryParse(s, out int sonuc);
+
+            int deneme = 9;
+            sonuc = F2(ref deneme);
+            Console.WriteLine("Sonrası: " + sonuc);
+            Console.WriteLine("Sonuç:  " + sonuc);
+            #endregion
+
+        }
+    }
+}
---
+++ Methods_5.12_6.12/MethotOverloading/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace MethodOverloading_6._12
+{
+    internal class Program
+    {
+        /*  
+         * Method Overloading (Metot Aşırı yükleme) : AYNI İSİMDE fakat farklı imzada olan metodlar oluşturmaktır.
+         */
+        static int Mutlak(int sayi) //sayi integer ise
+        {
+            if (sayi < 0)
+            {
+                return -1 * sayi;
+            }
+            return sayi;
+        }
+        static double Mutlak(double sayi) //sayi double ise
+        {
+            if (sayi < 0)
+            {  
+                return -1 * sayi;
+            }
+            return sayi;
+        }
+
+        static double BizimMetot(double sayi, int derece) //aşağıdaki verilen (-8, 3)'ün bu metotta nasıl çalıştığı
+        {
+            while (derece < 1) //derece şu anda 3 verildiği için while döngüsüne girmez, eğer 0 veya -li bir sayı girilseydi while döngüsüne giricek ve yeni bir derece isteyecekti
+            {
+                Console.WriteLine("Dereceyi tam pozitif ve 1 den büyük giriniz");
+                derece = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (sayi < 0) //sayı -8 olduğu için if'e girer ama...
+            {
+                while (derece % 2 == 0) //derece 3 olduğu ve 3%2!==0 olduğu için bu while'a girmez ve aşağıdaki if'ten devam eder.
+                {
+                    Console.WriteLine("Sayı negatif ise derece çift olamaz! Ayrıca derece her zaman pozitif olmalıdır");
+                    derece = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            if (sayi < 0) //(-8, 3) burda işlem görücek.
+            {
+                sayi = Math.Abs(sayi);//Sayının mutlak değerini döndüren fonksiyon. -8'i 8 yapıcak
+                return Math.Pow(sayi, 1 / derece) * -1; //8'in 1/3 denilen şey 8'in '∛8' halidir yani 2 döndürüp -1 ile çarpıcak ve bunu return edicek. 
+            }
+            return Math.Pow(1,0/ derece);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(64)); //64'ün kökünü döndüren fonksiyon
+            Console.WriteLine(Math.Pow(2, -6)); //2'nin -6'ncı üssünü döndüren fonksiyon
+            Console.WriteLine(Math.Ceiling(2.16)); //sayıyı en yakın üst tamsayıya yuvarlıyor. Yani bu durumda 3
+            Console.WriteLine(Math.Floor(2.96)); //sayıyı en yakın alt tamsayıya yuvarlıyor. Yani bu durumda 2
+            Console.WriteLine(Math.Max(13, 6)); //iki sayı arasındaki max değer
+            Console.WriteLine(Math.Min(13, 6)); //iki sayı arasındaki min değer
+            Console.WriteLine(Math.PI); //Pi sayısı
+            Console.WriteLine(Math.E); //e sayısı
+            Console.WriteLine(Math.Sin(30 * Math.PI / 180)); //Sinüs fonksiyonu 30*3,14/180
+            Console.WriteLine(BizimMetot(-8, 3));
+            Console.WriteLine(Mutlak(-14));//aynı isimde ama int döndüren mutlak
+            Console.WriteLine(Mutlak(-28.93));//aynı isimde ama double döndüren mutlak method overloading'in en basit olarak yaptığı şey budur.
+                                              //İsim aynı ama farklı değeri olan, farklı parametre alan veya farklı sonuç döndüren methodlara denir.
+        }
+    }
+}
